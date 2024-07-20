67918bf9edd2099f73ac57006af98aeb5cf66258
PhatNguyen2001 <caophat225@gmail.com>
2024-07-21 00:47:59 +0700

    chore deploy file run docker compose build

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ git pull origin main
 
 # Xây dựng lại các container Docker
 docker-compose down
-# docker-compose build
-docker-compose up -d 
-#testingdeploy
+
+docker-compose up --build -d 
+
 # In ra thông báo hoàn tất
 echo "Triển khai hoàn tất thành công!"
