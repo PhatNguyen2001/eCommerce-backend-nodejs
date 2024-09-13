6a54932516db1343bfe1a68da936906795bb26e7
PhatNguyen2001 <caophat225@gmail.com>
2024-09-14 03:27:43 +0700

    add Ci

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import app from "./src/app.js";
+import dotenv from 'dotenv';
 
+dotenv.config();
 const port = 3055 
 
-const server = app.listen(port, () => {
-    console.log(`app listion on port:${port}`);
+const server = app.listen(process.env.PORT || port, () => {
+    console.log(`app listion on port:${process.env.PORT}`);
 })
 
 process.on('SIGINT', () => {
